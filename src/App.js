2b093ebeec0b69208ab5ad6ed11e64eeab0f496c
mkanaet <mkanaet@gmail.com>
2024-04-19 00:19:00 +0200

    New commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './components/pages/Home';
 import Bookings from './components/pages/Bookings';
 import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
 import NotFound from './components/pages/NotFound';
-import UnderConstruction from './components/pages/UnderConstruction';
+
 
 const App = () => {
   return (
@@ -16,27 +16,13 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path={pages.get('home').path} element={<Home />} />
-          <Route
-            path={pages.get('about').path}
-            element={<UnderConstruction />}
-          />
-          <Route
-            path={pages.get('menu').path}
-            element={<UnderConstruction />}
-          />
+
           <Route path={pages.get('bookings').path} element={<Bookings />} />
           <Route
             path={pages.get('confirmedBooking').path}
             element={<ConfirmedBooking />}
           />
-          <Route
-            path={pages.get('orderOnline').path}
-            element={<UnderConstruction />}
-          />
-          <Route
-            path={pages.get('login').path}
-            element={<UnderConstruction />}
-          />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
